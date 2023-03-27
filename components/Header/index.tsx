f1da0d91292f3f218fb2d4fb9a4d36e9bddf8276
Rafael do Carmo <rafaelcarmo143@gmail.com>
2023-03-27 00:38:24 -0300

    fix: responsive

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <nav className="bg-black">
-      <div className="container text-white mx-auto h-20 flex justify-between items-center">
+      <div className="container text-white mx-auto h-20 flex justify-between items-center px-5 md:p-0">
         <span className="font-black">Logo</span>
         <nav>ICONE</nav>
       </div>
