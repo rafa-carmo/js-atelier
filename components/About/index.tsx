28d96e7ea4fb9516f230eb1668a0c3f40677a2aa
Rafael do Carmo <rafaelcarmo143@gmail.com>
2023-03-27 00:42:50 -0300

    About responsive

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -2,11 +2,11 @@ import { Heading } from '@comp/Heading'
 
 export function About() {
   return (
-    <div className="container py-16 mx-auto w-full flex items-center justify-center gap-28">
-      <div className="max-w-md flex flex-col gap-4">
-        <div className="flex flex-col gap-2">
+    <div className="container py-16 px-16 mx-auto w-full flex flex-col md:flex-row items-center justify-between gap-10 md:gap-28 relative">
+      <div className="md:max-w-full max-w-xl flex flex-col gap-4">
+        <div className="flex flex-col gap-16 sm:gap-2">
           <Heading>Sobre</Heading>
-          <h3 className="font-bold text-2xl">Bem Vindo a JS Atelier</h3>
+          <h3 className="font-bold text-3xl">Bem Vindo a JS Atelier</h3>
         </div>
         <p className="text-sm">
           Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
@@ -22,7 +22,7 @@ export function About() {
       <img
         src="https://scontent-gig2-1.xx.fbcdn.net/v/t39.30808-6/223266080_2865183517067610_2744943178628486717_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=D-TRjxlP-igAX-BlxX9&_nc_ht=scontent-gig2-1.xx&oh=00_AfD0LGcr7z_xM2mC4Yp9QIHjLxGcw9__bIYsaVN2GXqi2A&oe=64255079"
         alt="IMAGE"
-        className="w-80 h-80 object-contain rounded"
+        className="md:w-80 md:h-80 w-32 h-32 object-contain rounded-full md:rounded md:relative absolute top-3 right-2"
       />
     </div>
   )
